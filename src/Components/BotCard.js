01300be28b3291651018e85440416287aa302c97
lawi <lawi.bett@student.moringaschool.com>
2023-07-30 13:31:54 +0300

    rendered each bot and added style

--- src/Components/BotCard.js
+++ src/Components/BotCard.js
@@ -1,24 +1,24 @@
-// import React from "react";
+import React from "react";
 
-// function BotCard({ bot }) {
-//   return (
-//     <div className="grid grid-cols-5 gap-4 p-4 bg-black rounded shadow-md">
-//       <div className="col-span-2">
-//         <img
-//           alt="OH NO"
-//           src={bot.avatar_url}
-//           className="w-full h-auto rounded-lg filter saturate-150 contrast-125"
-//         />
-//       </div>
-//       <div className="col-span-3 space-y-2">
-//         <h2 className="text-xl font-semibold">{bot.name}</h2>
-//         <p className="text-gray-600 line-clamp-5">{bot.catchphrase}</p>
-//         <p className="text-green-500 font-semibold">Health: {bot.health}</p>
-//         <p className="text-red-500 font-semibold">Damage: {bot.damage}</p>
-//         <p className="text-blue-500 font-semibold">Armor: {bot.armor}</p>
-//       </div>
-//     </div>
-//   );
-// }
+function BotCard({ bot }) {
+  return (
+    <div className="grid grid-cols-5 gap-4 p-4 bg-black rounded shadow-md">
+      <div className="col-span-2">
+        <img
+          alt="OH NO"
+          src={bot.avatar_url}
+          className="w-full h-auto rounded-lg filter saturate-150 contrast-125"
+        />
+      </div>
+      <div className="col-span-3 space-y-2">
+        <h2 className="text-xl font-semibold">{bot.name}</h2>
+        <p className="text-gray-600 line-clamp-5">{bot.catchphrase}</p>
+        <p className="text-green-500 font-semibold">Health: {bot.health}</p>
+        <p className="text-red-500 font-semibold">Damage: {bot.damage}</p>
+        <p className="text-blue-500 font-semibold">Armor: {bot.armor}</p>
+      </div>
+    </div>
+  );
+}
 
-// export default BotCard;
+export default BotCard;
