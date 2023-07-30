474263c0a80644738bef35d0122b2ff97eefbaa3
lawi <lawi.bett@student.moringaschool.com>
2023-07-30 13:29:10 +0300

    fethced data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import BotCollection from "./Components/BotCollection";
+
+
 
 function App() {
+  const [bots, setBots] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/bots")
+      .then((res) => res.json())
+      .then((data) => setBots(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen p-8">
+      <h1 className="text-3xl font-semibold mb-6">Welcome to Bot Collection</h1>
+      <BotCollection bots={bots} />
     </div>
   );
 }
