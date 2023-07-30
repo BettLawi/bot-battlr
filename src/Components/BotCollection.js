835cd501d9bc2bc7a8e1aca0f99aa75ba011111f
lawi <lawi.bett@student.moringaschool.com>
2023-07-30 13:30:09 +0300

    added botcollection component

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -1,16 +1,16 @@
-// import React from "react";
-// import BotCard from "./BotCard";
+import React from "react";
+import BotCard from "./BotCard";
 
-// function BotCollection({ bots }) {
-//   return (
-//     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
-//       {bots.map((bot) => (
-//         <div key={bot.id} className="flex justify-center">
-//           <BotCard bot={bot} />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
+function BotCollection({ bots }) {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
+      {bots.map((bot) => (
+        <div key={bot.id} className="flex justify-center">
+          <BotCard bot={bot} />
+        </div>
+      ))}
+    </div>
+  );
+}
 
-// export default BotCollection;
+export default BotCollection;
