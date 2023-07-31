986c57b11c7c0a3b5715eb070da6adfa10b443bd
lawi <lawi.bett@student.moringaschool.com>
2023-07-31 12:30:52 +0300

    styled each bot

--- src/Components/BotCard.js
+++ src/Components/BotCard.js
@@ -37,7 +37,7 @@ import React from "react";
 
 function BotCard({ bot, enlistBot }) {
   return (
-    <div className="grid grid-cols-5 gap-4 p-4 bg-white rounded shadow-md">
+    <div className="grid grid-cols-5 gap-4 p-4 bg-green-100 rounded shadow-md">
       <div className="col-span-2">
         <img
           alt="OH NO"
@@ -55,7 +55,7 @@ function BotCard({ bot, enlistBot }) {
           onClick={() => enlistBot(bot)}
           className="bg-green-500 text-white rounded px-4 py-2"
         >
-          Enlist
+          Add to army
         </button>
       </div>
     </div>
--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -1,22 +1,5 @@
-// import React from "react";
-// import BotCard from "./BotCard";
-
-// function BotCollection({ bots }) {
-//   return (
-//     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
-//       {bots.map((bot) => (
-//         <div key={bot.id} className="flex justify-center">
-//           <BotCard bot={bot} />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default BotCollection;
-
 import React from "react";
-import BotCard from "./BotCard"; // Assuming you already have a BotCard component
+import BotCard from "./BotCard"; 
 
 function BotCollection({ bots, enlistBot }) {
   return (
